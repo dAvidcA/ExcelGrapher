e6802518bb5681565aed29b30a620a4959cc2a9d
David Cardona <cardonada@gmail.com>
2019-12-16 01:59:56 -0500

    Graficas de barra SystemCost

---
+++ results/index.php
@@ -0,0 +1,42 @@
+﻿<!doctype html>
+<html>
+
+<head>
+	<title>Bar Chart</title>
+	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.3/xlsx.core.min.js"></script>
+	<script src="../lib/Chart.min.js"></script>
+	<script src="../lib/utils.js"></script>
+	<script src="js.js"></script>
+	<style>
+	canvas {
+		-moz-user-select: none;
+		-webkit-user-select: none;
+		-ms-user-select: none;
+	}
+	#namSheet{
+		position: relative;
+		float: right;
+		font-size: 12px;
+		font-weight: bold;
+	}
+	#namSheet input{
+		font-size: 12px;
+		font-weight: normal;
+	}
+	.inputfile:focus + label {
+		outline: 1px dotted #000;
+		outline: -webkit-focus-ring-color auto 5px;
+	}
+	</style>
+</head>
+
+<body>
+	<div id="namSheet">Sheet name: <input type="text" id="nameSheet" value="SystemCost"></div>
+	<input type="file" id="files" name="files" multiple/> 
+	<div id="container" style="widthbarChartData: 75%;">
+		<canvas id="canvas"></canvas>
+	</div>
+</body>
+
+</html>
---
+++ results/js.js
@@ -0,0 +1,78 @@
+const range = (start, end, step) => {
+  return Array.from(Array.from(Array(Math.ceil((end-start)/step)).keys()), x => start+ x*step);
+}
+var barChartData = { labels: range(1, 25, 1) };
+
+$(document).ready(function(){	
+	$('#files').change(handleFile);	
+	var ctx = document.getElementById('canvas').getContext('2d');
+	window.myBar = new Chart(ctx, {
+		type: 'bar',
+		data: barChartData,
+		options: {
+			responsive: true,
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: 'Bar Chart'
+			}
+		}
+	});
+});
+
+function handleFile(e) {	
+	emptyData();
+	var files = e.target.files;
+	for (var i = 0; i != files.length; i++) {
+		f = files[i];
+		var reader = new FileReader();
+		var name = f.name;
+		reader.onload = function (e) {
+			var data = e.target.result;
+			var workbook = XLSX.read(data, { type: 'binary' });			
+			var sheet_name_list = workbook.SheetNames;
+			
+			var nameSheet = $('#nameSheet').val();
+			var dataFile = XLSX.utils.sheet_to_json(workbook.Sheets[nameSheet]);
+			
+			if (dataFile.length > 0) {
+				renderGraph([{name: name, data: dataFile}]);
+			}
+		};
+		reader.readAsBinaryString(f);
+	}
+}
+
+function renderGraph(graphData) {
+	console.log(graphData);
+	var color = Chart.helpers.color;
+	var colorNames = Object.keys(window.chartColors);
+	var colorName = colorNames[barChartData.datasets.length % colorNames.length];
+	
+	$.each(graphData,function(i,info){
+		var dsColor = window.chartColors[colorName];
+		var newDataset = {
+			label: info.name,
+			backgroundColor: color(dsColor).alpha(0.5).rgbString(),
+			borderColor: dsColor,
+			borderWidth: 1,
+			data: []
+		};
+		
+		var dataFile = info.data;
+		for (var index = 0; index < barChartData.labels.length; ++index) {
+			var tmpNum = parseFloat(dataFile[index].SystemCost).toFixed(2);
+			newDataset.data.push(tmpNum);
+		}
+
+		barChartData.datasets.push(newDataset);
+	});
+	window.myBar.update();
+}
+
+function emptyData(){
+	barChartData.datasets = [];
+	window.myBar.update();
+}
